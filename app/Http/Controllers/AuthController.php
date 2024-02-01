79daa48562b5e047ea9119b47356eacf46a47ad2
Amgadezzat-andro <megostar59@gmail.com>
2024-02-02 01:05:55 +0200

    Add user_type to database and make migrate again Make Login Check Which Roles Make Admin , Parent , Teacher , Student , Middlewares Assign Middlewares to Kernal Make Middlewares groups in routes For Each Role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,15 +11,40 @@ class AuthController extends Controller
     public function login()
     {
         if (!empty(Auth::check())) {
-            return redirect('admin/dashboard');
+            if (Auth::user()->user_type == 1) {
+                return redirect('admin/dashboard');
+
+            } else if (Auth::user()->user_type == 2) {
+                return redirect('teacher/dashboard');
+
+            } else if (Auth::user()->user_type == 3) {
+                return redirect('student/dashboard');
+
+            } else if (Auth::user()->user_type == 4) {
+                return redirect('parent/dashboard');
+            }
+        } else {
+            return view('auth.login');
         }
-        return view('auth.login');
     }
     public function AuthLogin(Request $request)
     {
         $remember = !empty($request->remember) ? true : false;
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember)) {
-            return redirect('admin/dashboard');
+
+            if (Auth::user()->user_type == 1) {
+                return redirect('admin/dashboard');
+
+            } else if (Auth::user()->user_type == 2) {
+                return redirect('teacher/dashboard');
+
+            } else if (Auth::user()->user_type == 3) {
+                return redirect('student/dashboard');
+
+            } else if (Auth::user()->user_type == 4) {
+                return redirect('parent/dashboard');
+
+            }
 
         } else {
             return redirect()->back()->with('error', 'Incorrect Username Or Password');
--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,45 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 }); */
 
+// Open Login Page & Check which Role
 Route::get('/', [AuthController::class, 'login']);
+// Whenever Click Login
 Route::post('login', [AuthController::class, 'AuthLogin']);
+// Whenever Click Logout
 Route::get('logout', [AuthController::class, 'logout']);
 
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard');
+
+
+// MiddleWares Groups
+
+Route::group(['middleware' => 'admin'], function () {
+
+    Route::get('admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+    Route::get('admin/admin/list', function () {
+        return view('admin.admin.list');
+    });
+
+
 });
 
-Route::get('admin/admin/list', function () {
-    return view('admin.admin.list');
-});
+Route::group(['middleware' => 'teacher'], function () {
+    Route::get('teacher/dashboard', function () {
+        return view('teacher.dashboard');
+    });
 
+});
+Route::group(['middleware' => 'student'], function () {
+    Route::get('student/dashboard', function () {
+        return view('student.dashboard');
+    });
+
+});
+Route::group(['middleware' => 'parent'], function () {
+    Route::get('parent/dashboard', function () {
+        return view('parent.dashboard');
+    });
+
+});
