700e84a645e45a6615493d1189d6dfb021bf4cf1
Amgadezzat-andro <megostar59@gmail.com>
2024-02-02 19:48:33 +0200

    Make Search Admin Proccess Assingn bootstrap to paginator in AppServiceProvider Add Search Form With date , name , email

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Request;
 use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
@@ -54,11 +57,22 @@ class User extends Authenticatable
 
     static public function getAdmin()
     {
-        return self::select('users.*')
-                    ->where('user_type', '=', 1)
-                    ->where('is_delete', '=', 0)
-                    ->orderBy('id', 'desc')
-                    ->get();
+        $return = self::select('users.*')
+            ->where('user_type', '=', 1)
+            ->where('is_delete', '=', 0);
+        if (!empty(Request::get('name'))) {
+            $return = $return->where('name', 'like', '%' . Request::get('name') . '%');
+        }
+        if (!empty(Request::get('email'))) {
+            $return = $return->where('email', 'like', '%' . Request::get('email') . '%');
+        }
+        if (!empty(Request::get('date'))) {
+            $return = $return->whereDate('created_at', '=', Request::get('date'));
+        }
+        $return = $return->orderBy('id', 'desc')
+            ->paginate(2);
+
+        return $return;
     }
     static public function getOneAdmin($adminID)
     {
--- resources/views/admin/admin/list.blade.php
+++ resources/views/admin/admin/list.blade.php
@@ -15,7 +15,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Admin List</h1>
+                            <h1>Admin List Total : {{ $admins->total() }} Admins</h1>
                         </div>
                         <div class="col-sm-6" style="text-align: right;">
                             <a href="{{ url('admin/admin/add') }}" class="btn btn-primary">Add New Admin</a>
@@ -24,6 +24,51 @@
                     </div>
                 </div><!-- /.container-fluid -->
             </section>
+
+
+
+
+            <!-- Search Form -->
+            <div class="card card-primary">
+                <!-- form start -->
+                <form action="" method="get">
+                    <div class="card-header">
+                        <h3 class="card-title">Search Admin</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="form-group col-md-3">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="Enter Admin's Name"
+                                    value="{{ Request::get('name') }}">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label>Email address</label>
+                                <input type="text" class="form-control" name="email" placeholder="Enter Admin's email"
+                                    value="{{ Request::get('email') }}">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label>Date</label>
+                                <input type="date" class="form-control" name="date"
+                                    placeholder="Enter Admin's created Date" value="{{ Request::get('date') }}">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <button class="btn btn-primary" type="submit" style="margin-top: 30px">Search</button>
+                                <a href="{{ url('admin/admin/list') }}" class="btn btn-success"
+                                    style="margin-top: 30px">Clear</a>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- /.card-body -->
+                </form>
+            </div>
+            <!-- /.card -->
+
+
+
+
 
             {{-- Table Card --}}
             <div class="card">
@@ -48,7 +93,7 @@
                                     <td>{{ $admin->id }}</td>
                                     <td>{{ $admin->name }}</td>
                                     <td>{{ $admin->email }}</td>
-                                    <td>{{ $admin->created_at }}</td>
+                                    <td>{{ date('d-m-Y', strtotime($admin->created_at)) }}</td>
                                     <td>
                                         <a href="{{ url('admin/admin/edit/' . $admin->id) }}"
                                             class="btn btn-primary">Edit</a>
@@ -59,6 +104,10 @@
                             @endforeach
                         </tbody>
                     </table>
+
+                    <div style="padding: 10px; float: right; ">
+                        {{ $admins->appends(request()->query())->links() }}
+                    </div>
                 </div>
                 <!-- /.card-body -->
             </div>
